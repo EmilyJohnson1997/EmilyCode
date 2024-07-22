b722f3ab8162a8cfa759858b513804763d54a147
EmilyJohnson1997 <emily_johnson_888@163.com>
2024-07-23 00:28:26 +0900

    Create fun_py_calculate_cone_volume.py

---
+++ Code/fun_py_calculate_cone_volume.py
@@ -0,0 +1,7 @@
+# fun_py_calculate_cone_volume.py
+import math
+
+def fun_py_calculate_cone_volume(radius, height):
+    return (1/3) * math.pi * radius ** 2 * height
+
+print(fun_py_calculate_cone_volume(3, 5))
